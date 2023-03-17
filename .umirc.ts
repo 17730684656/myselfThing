3f33a0bc7db074920fb54e50bb95253a779e5d1f
17730684656 <867088577@qq.com>
2023-03-17 16:47:24 +0800

    chang config

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: 'Site Name',
   mode: 'site',
+   publicPath: '/myselfThing/',
+  base: '/ myselfThing',
   // more config: https://d.umijs.org/config
 });
